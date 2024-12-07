98fae7568cebf3877c727220bd5b8f381554c635
Hameed Shirzay <hameedshirzay2@gmail.comgit config --global user.email hameedshirzay2@gmail.comgit config --listcdpwe>
2024-12-07 13:35:56 +0100

    Task4_test.py update

--- Test automation/Selenium Web Elements and PyTest/Task4_test.py
+++ Test automation/Selenium Web Elements and PyTest/Task4_test.py
@@ -14,6 +14,42 @@ Explore the PyTest basics.
 # 2. pytest --version
 
 
-# write a basic test:
+# writing a basic test:
 def test_addition():
     assert 2 + 2 == 4
+
+
+#Writing Assertions with some examples:
+
+# 1. checking wiht some values:
+def test_sum():
+    assert 1 + 2 == 3  # Pass
+    assert 2 + 2 == 5  # Fail
+
+# 2.checking list:
+def test_list():
+    assert [1, 2, 3] == [1, 2, 3]  # Pass
+    assert [1, 2, 3] == [3, 2, 1]  # Fail
+
+# 3. Checking Exceptions:
+def test_raises_error():
+    with pytest.raises(ZeroDivisionError):
+        1 / 0  # Pass
+
+
+#Using Pytest Fixtures:
+@pytest.fixture
+def sample_data():
+    return [1, 2, 3]
+
+def test_data_length(sample_data):
+    assert len(sample_data) == 3
+
+# Parametrizing Tests:
+@pytest.mark.parametrize("a, b, expected", [
+    (1, 2, 3),
+    (2, 3, 5),
+    (3, 5, 8),
+])
+def test_add(a, b, expected):
+    assert a + b == expected
