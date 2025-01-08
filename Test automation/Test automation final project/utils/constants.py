928e6fdebd8c8f9899c759f10c60f3f53c55f700
HameedShirzay <hameedshirzay2@gmail.com>
2025-01-08 10:08:46 +0100

    Add files via upload

---
+++ Test automation/Test automation final project/utils/constants.py
@@ -0,0 +1,13 @@
+BASE_URL = "https://grocerymate.masterschool.com"
+PRODUCT_URL = f"{BASE_URL}/store"
+
+users = [
+    {"email": "user1@example.com", "password": "1d3f1db63842ba"},
+    {"email": "user2@example.com", "password": "1d3f1db63842ba"},
+    {"email": "user3@example.com", "password": "1d3f1db63842ba"},
+    {"email": "user4@example.com", "password": "1d3f1db63842ba"},
+    {"email": "user5@example.com", "password": "1d3f1db63842ba"}
+]
+
+TODAY_DATE_MINUS_18 = "06/01/2007"
+TODAY_DATE_MINUS_17 = "06/01/2008"
